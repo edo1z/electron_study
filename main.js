ed62009c72e2943eae5ee9f074cfe3035443e412
edo1z <endo.yuta@logicky.com>
2018-10-07 21:21:37 +0900

    first

---
+++ main.js
@@ -0,0 +1,15 @@
+const {app, BrowserWindow} = require('electron')
+
+let win
+
+function createWin() {
+  win = new BrowserWindow({width: 500, height: 500})
+  win.loadFile('index.html')
+  win.on('closed', () => win = null)
+}
+
+app.on('ready', createWin)
+app.on('window-all-closed', () => app.quit())
+app.on('activate', () => {
+  if (win === null) createWin()
+})
