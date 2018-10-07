900efce60953f9638456d60cd204b4f3f180267c
edo1z <endo.yuta@logicky.com>
2018-10-07 23:00:08 +0900

    add client view

--- server.js
+++ server.js
@@ -6,10 +6,16 @@ let serverStatus = false
 const start = () => {
   if (!serverStatus) {
     console.log('server starting...')
+    route(app)
     serverStatus = true
     server.listen(3000, () => console.log('listening on *:3000'))
     calc.start()
   }
+}
+const route = (app) => {
+  app.get('/', function(req, res){
+    res.sendFile(__dirname + '/client.html');
+  });
 }
 const stop = () => {
   if (serverStatus) {
