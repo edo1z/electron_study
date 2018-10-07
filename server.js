1885184588ab906a61930438d24599d1940d4f77
edo1z <endo.yuta@logicky.com>
2018-10-07 22:29:00 +0900

    added count timer

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require('express')()
 const server = require('http').Server(app)
+const calc = require('./calc')
 
 let serverStatus = false
 const start = () => {
@@ -7,10 +8,12 @@ const start = () => {
     console.log('server starting...')
     serverStatus = true
     server.listen(3000, () => console.log('listening on *:3000'))
+    calc.start()
   }
 }
 const stop = () => {
   if (serverStatus) {
+    calc.stop()
     server.close()
     serverStatus = false
     console.log('server stopped')
